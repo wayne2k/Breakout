0e8d72ca99ea55ef91fbfd0965ad9a39cea544bb
wayne2k <wayne2k@gmail.com>
2015-01-30 19:32:28 +0200

    Enviroment Setup
    Enviroment Setup.
    Scripts Templates Created.
    Paddle Script Written.

---
+++ Assets/Breakout/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RollRoti.Breakout
+{
+	public class Paddle : MonoBehaviour
+	{
+		public float paddleSpeed = 1f;
+
+		Vector3 playerPos = new Vector3 (0f, -9.5f, 0f);
+
+		void Update ()
+		{
+			float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed * Time.deltaTime);
+
+			playerPos = new Vector3 (Mathf.Clamp (xPos, -8f, 8f), -9.5f, 0f);
+
+			transform.position = playerPos;
+		}
+	}
+}
---
+++ Assets/Breakout/Scripts/TimedDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RollRoti.Breakout
+{
+	public class TimedDestroy : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
